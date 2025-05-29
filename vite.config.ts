8b0a00a18bac6174580cb155e23a32e91773fc2d
David Park <gudrlf46@gmail.com>
2025-05-29 17:13:48 +0900

    vite 설정 재수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
@@ -9,7 +8,12 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: '::',
     port: 8080,
-    allowedHosts: ["2e2f1706-c6a1-48a6-ae32-e81be4d39721.lovableproject.com", "https://peermall.com"]
+    allowedHosts: ["2e2f1706-c6a1-48a6-ae32-e81be4d39721.lovableproject.com"]
+  },
+  preview: {
+    host: '::',
+    port: 4173,
+    allowedHosts: ["peermall.com"]
   },
   plugins: [react(), mode === 'development' && componentTagger()].filter(
     Boolean
